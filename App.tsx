60b9062c8b4d043243e121618cadcc4ce736d828
Nikita Konovalov <n.konovalov@galament.com>
2021-05-29 08:48:05 +0400

    use loading references

--- App.tsx
+++ App.tsx
@@ -1,23 +1,14 @@
 import React, { FC, useState } from 'react';
 import { Text, View } from 'react-native';
-
 import AppLoading from 'expo-app-loading';
-import * as Font from 'expo-font';
 
-async function loadApplication() {
-    await Font.loadAsync({
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        'openSans-regular': require('./assets/fonts/OpenSans-Regular.ttf'),
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        'openSans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
-    });
-}
+import bootstrap from './src/bootstrap';
 
 const App: FC = () => {
     const [isReady, setIsReady] = useState(false);
 
     if (!isReady) {
-        return <AppLoading startAsync={loadApplication} onError={console.log} onFinish={() => setIsReady(true)} />;
+        return <AppLoading startAsync={bootstrap} onError={console.log} onFinish={() => setIsReady(true)} />;
     }
 
     return (
