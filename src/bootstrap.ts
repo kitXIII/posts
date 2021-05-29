8093b04852c8afc8764391fddcd94ce1ece31e1b
Nikita Konovalov <n.konovalov@galament.com>
2021-05-29 18:50:04 +0400

    fix fonts names

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -3,9 +3,9 @@ import * as Font from 'expo-font';
 const bootstrap = async (): Promise<void> => {
     await Font.loadAsync({
         // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        'openSans-regular': require('../assets/fonts/OpenSans-Regular.ttf'),
+        'open-regular': require('../assets/fonts/OpenSans-Regular.ttf'),
         // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        'openSans-bold': require('../assets/fonts/OpenSans-Bold.ttf')
+        'open-bold': require('../assets/fonts/OpenSans-Bold.ttf')
     });
 };
 
