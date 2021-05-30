d4db51af3f8ac46ff65c5392b0824e32929dce52
Nikita Konovalov <n.konovalov@galament.com>
2021-05-30 19:15:18 +0400

    add Post screen markup

--- src/Screens/PostScreen.tsx
+++ src/Screens/PostScreen.tsx
@@ -1,26 +1,50 @@
 import React, { FC } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Image, Text, Button, ScrollView, Alert } from 'react-native';
 
 import { ROUTES } from '../routes';
+import { THEME } from '../theme';
+import { DATA } from '../data';
+
 import type { ScreenNavigationProps } from '../Navigation/AppNavigation';
 
 type Props = ScreenNavigationProps<ROUTES.POST>;
 
 const PostScreen: FC<Props> = ({ route }: Props) => {
     const { postId } = route.params;
+    const post = DATA.find((p) => p.id === postId);
+
+    const removeHandler = () =>
+        Alert.alert(
+            'Post deletion',
+            'Are you sure?',
+            [
+                { text: 'Cancel', style: 'cancel' },
+                { text: 'Delete', style: 'destructive', onPress: () => console.log('Delete Pressed') }
+            ],
+            { cancelable: false }
+        );
     return (
-        <View style={styles.center}>
-            <Text>{postId}</Text>
-        </View>
+        <ScrollView>
+            <Image style={styles.image} source={{ uri: post?.img }} />
+            <View style={styles.textWrap}>
+                <Text style={styles.text}>{post?.text}</Text>
+            </View>
+            <Button title='Delete' color={THEME.COLOR_DANGER} onPress={removeHandler} />
+        </ScrollView>
     );
 };
 
 const styles = StyleSheet.create({
-    center: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
+    image: {
+        width: '100%',
+        height: 200
+    },
+    textWrap: {
+        padding: 10
+    },
+    text: {
+        fontFamily: THEME.FONT_REGULAR
     }
-})
+});
 
 export default PostScreen;
