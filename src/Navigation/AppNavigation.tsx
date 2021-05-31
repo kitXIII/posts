e664e814daf3e77d079ff8c225902a2dc60be78d
Nikita Konovalov <n.konovalov@galament.com>
2021-05-31 22:50:05 +0400

    add bottom tabs

--- src/Navigation/AppNavigation.tsx
+++ src/Navigation/AppNavigation.tsx
@@ -1,14 +1,17 @@
 import React, { FC } from 'react';
 import { Platform } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, StackNavigationOptions } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import MainScreen from '../Screens/MainScreen';
 import PostScreen from '../Screens/PostScreen';
-import CreateScreen from '../Screens/CreateScreen';
+// import CreateScreen from '../Screens/CreateScreen';
 import BookedScreen from '../Screens/BookedScreen';
-import AboutScreen from '../Screens/AboutScreen';
+// import AboutScreen from '../Screens/AboutScreen';
 
 import AppHeaderIcon from '../Components/AppHeaderIcon';
 
@@ -20,14 +23,19 @@ import type { PostType } from '../types';
 
 type StackParamList = {
     [ROUTES.MAIN]: undefined;
+    [ROUTES.BOOKED]: undefined;
     [ROUTES.POST]: {
         postId: PostType['id'];
         date: PostType['date'];
         booked: PostType['booked'];
     };
     [ROUTES.CREATE]: undefined;
-    [ROUTES.BOOKED]: undefined;
     [ROUTES.ABOUT]: undefined;
+};
+
+type TabParamList = {
+    [ROUTES.MAIN_SCREEN]: undefined;
+    [ROUTES.BOOKED_SCREEN]: undefined;
 };
 
 export type ScreenNavigationProps<RouteName extends keyof StackParamList = keyof StackParamList> = StackScreenProps<
@@ -35,72 +43,103 @@ export type ScreenNavigationProps<RouteName extends keyof StackParamList = keyof
     RouteName
 >;
 
-const Stack = createStackNavigator<StackParamList>();
+const commonScreenOptions = {
+    headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? THEME.COLOR_MAIN : THEME.COLOR_WHITE
+    },
+    headerTintColor: Platform.OS === 'android' ? THEME.COLOR_WHITE : THEME.COLOR_MAIN
+};
+
+const postScreenOptions = ({ route }: ScreenNavigationProps<ROUTES.POST>): StackNavigationOptions => {
+    const date = new Date(route.params.date).toLocaleDateString();
+    const starIconName = route.params.booked ? 'ios-star' : 'ios-star-outline';
+    return {
+        title: `Post dated ${date}`,
+        // eslint-disable-next-line react/display-name
+        headerRight: () => (
+            <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+                <Item title='Favorite' iconName={starIconName} onPress={() => console.log('Press favorite')} />
+            </HeaderButtons>
+        )
+    };
+};
+
+const BookedStack = createStackNavigator<StackParamList>();
+const BookedNavigator = () => (
+    <BookedStack.Navigator initialRouteName={ROUTES.BOOKED} screenOptions={commonScreenOptions}>
+        <BookedStack.Screen name={ROUTES.BOOKED} component={BookedScreen} />
+        <BookedStack.Screen name={ROUTES.POST} component={PostScreen} options={postScreenOptions} />
+    </BookedStack.Navigator>
+);
+
+const MainStack = createStackNavigator<StackParamList>();
+const MainNavigator = () => (
+    <MainStack.Navigator initialRouteName={ROUTES.MAIN} screenOptions={commonScreenOptions}>
+        <MainStack.Screen
+            name={ROUTES.MAIN}
+            component={MainScreen}
+            options={{
+                title: 'My blog',
+                // eslint-disable-next-line react/display-name
+                headerRight: () => (
+                    <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+                        <Item title='Take photo' iconName='ios-camera' onPress={() => console.log('Press photo')} />
+                    </HeaderButtons>
+                ),
+                // eslint-disable-next-line react/display-name
+                headerLeft: () => (
+                    <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+                        <Item title='Toggle drower' iconName='ios-menu' onPress={() => console.log('Press menu')} />
+                    </HeaderButtons>
+                )
+            }}
+        />
+        <MainStack.Screen name={ROUTES.POST} component={PostScreen} options={postScreenOptions} />
+        {/* <Stack.Screen name={ROUTES.CREATE} component={CreateScreen} />
+        <Stack.Screen name={ROUTES.ABOUT} component={AboutScreen} /> */}
+    </MainStack.Navigator>
+);
+
+type tabBarIconPropType = {
+    focused: boolean;
+    color: string;
+    size: number;
+};
+
+const BottomTab = createBottomTabNavigator<TabParamList>();
+const BottomTabNavigator = () => (
+    <BottomTab.Navigator
+        screenOptions={{
+            tabBarActiveTintColor: THEME.COLOR_MAIN
+        }}
+    >
+        <BottomTab.Screen
+            name={ROUTES.MAIN_SCREEN}
+            component={MainNavigator}
+            options={{
+                title: 'Main',
+                headerShown: false,
+                // eslint-disable-next-line react/display-name
+                tabBarIcon: ({ color }: tabBarIconPropType) => <Ionicons name='ios-albums' size={25} color={color} />
+            }}
+        />
+        <BottomTab.Screen
+            name={ROUTES.BOOKED_SCREEN}
+            component={BookedNavigator}
+            options={{
+                title: 'Favorites',
+                headerShown: false,
+                // eslint-disable-next-line react/display-name
+                tabBarIcon: ({ color }: tabBarIconPropType) => <Ionicons name='ios-star' size={25} color={color} />
+            }}
+        />
+    </BottomTab.Navigator>
+);
 
 const AppNavigation: FC = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator
-                initialRouteName={ROUTES.MAIN}
-                screenOptions={{
-                    headerStyle: {
-                        backgroundColor: Platform.OS === 'android' ? THEME.COLOR_MAIN : THEME.COLOR_WHITE
-                    },
-                    headerTintColor: Platform.OS === 'android' ? THEME.COLOR_WHITE : THEME.COLOR_MAIN
-                }}
-            >
-                <Stack.Screen
-                    name={ROUTES.MAIN}
-                    component={MainScreen}
-                    options={{
-                        title: 'My blog',
-                        // eslint-disable-next-line react/display-name
-                        headerRight: () => (
-                            <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
-                                <Item
-                                    title='Take photo'
-                                    iconName='ios-camera'
-                                    onPress={() => console.log('Press photo')}
-                                />
-                            </HeaderButtons>
-                        ),
-                        // eslint-disable-next-line react/display-name
-                        headerLeft: () => (
-                            <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
-                                <Item
-                                    title='Toggle drower'
-                                    iconName='ios-menu'
-                                    onPress={() => console.log('Press menu')}
-                                />
-                            </HeaderButtons>
-                        )
-                    }}
-                />
-                <Stack.Screen
-                    name={ROUTES.POST}
-                    component={PostScreen}
-                    options={({ route }) => {
-                        const date = new Date(route.params.date).toLocaleDateString();
-                        const starIconName = route.params.booked ? 'ios-star' : 'ios-star-outline';
-                        return {
-                            title: `Post dated ${date}`,
-                            // eslint-disable-next-line react/display-name
-                            headerRight: () => (
-                                <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
-                                    <Item
-                                        title='Favorite'
-                                        iconName={starIconName}
-                                        onPress={() => console.log('Press favorite')}
-                                    />
-                                </HeaderButtons>
-                            )
-                        };
-                    }}
-                />
-                <Stack.Screen name={ROUTES.CREATE} component={CreateScreen} />
-                <Stack.Screen name={ROUTES.BOOKED} component={BookedScreen} />
-                <Stack.Screen name={ROUTES.ABOUT} component={AboutScreen} />
-            </Stack.Navigator>
+            <BottomTabNavigator />
         </NavigationContainer>
     );
 };
--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,9 @@
 export enum ROUTES {
     MAIN = 'Main',
+    MAIN_SCREEN = 'Main screen',
     POST = 'Post',
     CREATE = 'Create',
     BOOKED = 'Booked',
+    BOOKED_SCREEN = 'Booked screen',
     ABOUT = 'About'
 }
